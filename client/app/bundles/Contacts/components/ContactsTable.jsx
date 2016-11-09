e2c973535cafdb8bf6a339a448bba5e000a08296
Megan Coyle <meg@mcoyle.com>
2016-11-09 11:54:24 -0500

    Add React filters

--- client/app/bundles/Contacts/components/ContactsTable.jsx
+++ client/app/bundles/Contacts/components/ContactsTable.jsx
@@ -2,13 +2,34 @@ import React, { PropTypes } from 'react';
 
 export default class ContactsTable extends React.Component {
   static propTypes = {
-    contacts: PropTypes.array.isRequired,
+    contacts: PropTypes.array.isRequired
   };
 
   render() {
+    const contacts = this.props.contacts;
+
     return (
       <div>
-        Table!
+        <table>
+          <tbody>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Email Address</th>
+              <th>Phone Number</th>
+              <th>Company Name</th>
+            </tr>
+            {contacts.map((contact, i) => (
+            <tr key={i}>
+              <td>{contact.first_name}</td>
+              <td>{contact.last_name}</td>
+              <td>{contact.email_address}</td>
+              <td>{contact.phone_number}</td>
+              <td>{contact.company_name}</td>
+            </tr>
+          ))}
+          </tbody>
+        </table>
       </div>
     );
   }
--- client/app/bundles/Contacts/containers/ContactsTableContainer.jsx
+++ client/app/bundles/Contacts/containers/ContactsTableContainer.jsx
@@ -9,14 +9,97 @@ export default class ContactsTableContainer extends React.Component {
   constructor(props, context) {
     super(props, context);
 
-    this.state = { contacts: props.contacts };
+    this.state = {
+      contacts: props.contacts,
+      dotComOnly: false,
+      extensionOnly: false,
+      internationalOnly: false
+     };
+
+    this.sortBy = this.sortBy.bind(this);
+    this.toggleDotComOnly = this.toggleDotComOnly.bind(this);
+    this.toggleExtensionOnly = this.toggleExtensionOnly.bind(this);
+    this.toggleInternationalOnly = this.toggleInternationalOnly.bind(this);
   }
 
+  sortBy(key) {
+    const contacts = this.state.contacts;
+    const sorted = this.state.contacts.sort((contactA, contactB) => {
+      return contactA[key] >= contactB[key] ? 1 : -1;
+    });
+
+    this.setState({ contacts: sorted });
+  }
+
+  toggleDotComOnly(e) {
+    const dotComOnly = !this.state.dotComOnly;
+
+    const contacts = this.props.contacts.filter(contact => {
+      return !dotComOnly || contact.email_address.endsWith('.com');
+    })
+
+    this.setState({
+      dotComOnly,
+      contacts
+    })
+  }
+
+  toggleExtensionOnly(e) {
+    const extensionOnly = !this.state.extensionOnly;
+
+    const contacts = this.props.contacts.filter(contact => {
+      return !extensionOnly || contact.phone_number.includes('x');
+    })
+
+    this.setState({
+      extensionOnly,
+      contacts
+    })
+  }
+
+  toggleInternationalOnly(e) {
+    const internationalOnly = !this.state.internationalOnly;
+
+    const contacts = this.props.contacts.filter(contact => {
+      return !internationalOnly || contact.phone_number.includes('1-');
+    })
+
+    this.setState({
+      internationalOnly,
+      contacts
+    })
+  }
+
+
   render() {
-    const { contacts } = this.state;
+    const { contacts, dotComOnly, extensionOnly, internationalOnly } = this.state;
 
     return (
       <div>
+        <button
+          onClick={() => this.sortBy('email_address')}
+        >
+          Sort by email
+        </button>
+        <label>.com emails only</label>
+        <input
+          type="checkbox"
+          checked={dotComOnly}
+          onChange={this.toggleDotComOnly}
+        />
+        <label>extensions only</label>
+        <input
+          type="checkbox"
+          checked={extensionOnly}
+          onChange={this.toggleExtensionOnly}
+        />
+        <label>international only</label>
+        <input
+          type="checkbox"
+          checked={internationalOnly}
+          onChange={this.toggleInternationalOnly}
+        />
+
         <ContactsTable contacts={contacts} />
       </div>
     );
